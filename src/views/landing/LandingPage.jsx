fc3d44508b578fe06b6392ead1ec32b3cae404e6
larbi <99117566+larbi1512@users.noreply.github.com>
2024-10-19 18:27:26 +0100

    about us footer done

--- src/views/landing/LandingPage.jsx
+++ src/views/landing/LandingPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Navbar from './NavBar';
 import Hero from './Hero';
+import About from './About';
+import CallToAction from './CallToAction';
+import Footer from './Footer/Footer';
 
 function LandingPage() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center w-full ">
      <Hero/>
+     <About/>
+      <CallToAction/>
+      <Footer/>
+      
       
     </div>
   );
