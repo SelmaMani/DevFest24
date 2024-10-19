97fe47f097a20b7217631074c7914cce6008e812
Su-mia <soumia.bouaouina@ensia.edu.dz>
2024-10-19 21:51:10 +0100

    more in landing

--- src/views/landing/LandingPage.jsx
+++ src/views/landing/LandingPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './NavBar';
 import Hero from './Hero';
+import Features from './Features';
+import Pricing from './Pricing';
 
 function LandingPage() {
   return (
     <div>
      <Hero/>
+     <Features/>
+     <Pricing/>
       
     </div>
   );
